8ca52fba2e3f388fc591b2625b5c1248134de681
Jaideep D Naik <jaideepchinnu3@gmail.com>
2024-11-12 20:29:38 +0530

    Create BlurBackground.jsx

---
+++ src/components/BlurBackground.jsx
@@ -0,0 +1,83 @@
+import { motion } from "framer-motion";
+
+const circleVariants = {
+  animate1: {
+    scale: [1, 1.2, 1],
+    x: [0, 100, 0],
+    y: [0, 100, 0],
+    transition: {
+      duration: 2,
+      repeat: Infinity,
+      ease: "easeOut",
+    },
+  },
+
+  animate2: {
+    scale: [1, 1.1, 1],
+    x: [0, 120, 0],
+    y: [0, -60, 0],
+    transition: {
+      duration: 2,
+      repeat: Infinity,
+      ease: "easeInOut",
+    },
+  },
+
+  animate3: {
+    scale: [1, 1.3, 1],
+    x: [0, 140, 0],
+    y: [0, 70, 0],
+    transition: {
+      duration: 2,
+      repeat: Infinity,
+      ease: "easeInOut",
+    },
+  },
+
+  animate4: {
+    scale: [1, 1.4, 1],
+    x: [0, 160, 0],
+    y: [0, -80, 0],
+    transition: {
+      duration: 4,
+      repeat: Infinity,
+      ease: "easeInOut",
+    },
+  },
+
+  animate5: {
+    scale: [1, 1.5, 1],
+    x: [0, 100, 0],
+    y: [0, 90, 0],
+    transition: {
+      duration: 2,
+      repeat: Infinity,
+      ease: "easeInOut",
+    },
+  },
+
+  animate6: {
+    scale: [1, 1.6, 1],
+    x: [0, 200, 0],
+    y: [0, -100, 0],
+    transition: {
+      duration: 3,
+      repeat: Infinity,
+      ease: "easeInOut",
+    },
+  },
+};
+
+const BlurBackground = () => {
+  return (
+    <div className="fixed top-0 left-0 w-full h-full overflow-hidden -z-10 pointer-events-none flex justify-center items-center filter blur-[100px] opacity-70">
+      <motion.div
+        className="bg-yellow-500 w-[350px] h-[250px] rounded-full absolute"
+        variants={circleVariants}
+        animate="animate1"
+      ></motion.div>
+    </div>
+  );
+};
+
+export default BlurBackground;
