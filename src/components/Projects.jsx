e16875c4541d4fda27575e7786269ed99b649ffd
Jaideep D Naik <jaideepchinnu3@gmail.com>
2024-11-15 21:47:27 +0530

    Updated Projects.jsx. Template ready.

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,55 @@
-const Projects = () => {
-    return (
-        <div>
-            <h1>Projects</h1>
-        </div>
-    )
-}
+import { motion } from "framer-motion";
+import { PROJECTS } from "../constants";
 
-export default Projects
+const Projects = () => {
+  const projectVariants = {
+    hidden: { opacity: 0, scale: 0.8, rotate: -40, y: 50 },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      rotate: 0,
+      y: 0,
+      transition: {
+        duration: 0.6,
+        ease: "easeOut",
+        type: "spring",
+        bounce: 0.4,
+      },
+    },
+  };
+
+  return (
+    <section className="px-6 py-10" id="work">
+      <h1 className="text-4xl md:text-6xl font-medium tracking-tight mb-10">
+        Work
+      </h1>
+
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+        {PROJECTS.map((project, index) => (
+          <motion.div
+            key={index}
+            className="relative rounded-lg overflow-hidden h-[550px] transition transform"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            variants={projectVariants}
+          >
+            <img
+              src={project.image}
+              alt={project.name}
+              className="absolute inset-0 w-full h-full object-cover transition-opacity duration-300"
+            />
+            <div className="relative z-20 p-6 flex flex-col justify-between h-full bg-black/30 text-white">
+              <h2 className="text-2xl font-medium mb-4">{project.name}</h2>
+              <div className="flex flex-col justify-between">
+                <p className="mb-4 flex-grow text-2xl">{project.description}</p>
+              </div>
+            </div>
+          </motion.div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Projects;
